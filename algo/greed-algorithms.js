2091c344a66110e0150931b5c157422137072fe9
uladzislau.stuk <uladzislau.stuk@gmail.com>
2021-08-09 08:34:22 +0300

    Implement greed algorithms and linked list

--- algo/greed-algorithms.js
+++ algo/greed-algorithms.js
@@ -1,4 +1,129 @@
 /*
+* 23.07.2021
+* FIND THE MINIMUM COLORS THE MAP CAN BE PAINTED
+* TWO NEIGHBORING STATES CANNOT BE PAINTED THE SAME COLOR
+* */
+const map = [
+  [0,0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0],
+]
+
+function paintCell(map, rowIdx, colIdx, minNumberColors) {
+  if (rowIdx === 0 && colIdx === 0) {
+    map[rowIdx][colIdx] = 1
+    return minNumberColors
+  }
+  
+  const possiblePaths = [
+    // y            x
+    [rowIdx - 1, colIdx],     // top
+    [rowIdx - 1, colIdx + 1], // topRight
+    [rowIdx, colIdx + 1],     // right
+    [rowIdx + 1, colIdx + 1], // rightBottom
+    [rowIdx + 1, colIdx],     // bottom
+    [rowIdx + 1, colIdx - 1], // bottomLeft
+    [rowIdx, colIdx - 1],     // left
+    [rowIdx - 1, colIdx - 1 ] // leftTop
+  ]
+  
+  let currentColor = minNumberColors
+  
+  while (currentColor > 0 && currentColor <= minNumberColors) {
+    // check all possible paths
+    const canWePaintCell = possiblePaths.every(([checkedRowIdx, checkedColIdx]) => (
+      checkedRowIdx < 0 ||
+      checkedRowIdx >= map.length ||
+      checkedColIdx < 0 ||
+      checkedColIdx >= map[rowIdx].length ||
+      currentColor !== map[checkedRowIdx][checkedColIdx]
+    ))
+    
+    if (canWePaintCell) {
+      // paint cell
+      map[rowIdx][colIdx] = currentColor
+      return minNumberColors
+    }
+    
+    currentColor--
+  }
+  
+  // we wasn't able to find colIdxor, create new one
+  minNumberColors += 1
+  map[rowIdx][colIdx] = minNumberColors
+  
+  return minNumberColors
+}
+
+function findMinimumColorMapCanBePainted(map) {
+  if (!map.length) {
+    return 0
+  }
+  
+  let minNumberColors = 1
+  
+  for(let rowIdx = 0; rowIdx < map.length; rowIdx++) {
+    for (let colIdx = 0; colIdx < map[rowIdx].length; colIdx++) {
+      minNumberColors = paintCell(map, rowIdx, colIdx, minNumberColors)
+    }
+  }
+  
+  console.log(map)
+  
+  return minNumberColors
+}
+
+console.log(findMinimumColorMapCanBePainted(map))
+
+
+// 23.07.2021
+// FIND SUM OF NUMBER - O(n2)
+// 0 // 0
+// 0 + 1 // 0
+// 0 + 1 + 2 // 1
+// 0 + 1 + 2 + 3 // 2
+
+// Variables
+// 1 <= maxNumbersToSum <= numbers.length
+
+// 1. Find the minimum number
+// 2. Get previous sum and add min number
+// 3. Push index to already processed array
+
+function findSumOfNumbers() {
+  const numbers = [1,2,2,3,5,0,4,7]
+  // sums of min numbers
+  const sums = []
+  const processedIndexes = []
+  
+  for(let i = 0; i < numbers.length; i++) {
+    let minNumber = Infinity
+    let minNumberIndex
+    
+    for (let j = 0; j < numbers.length; j++ ) {
+      // find minimum number and skip already processed ones
+      if (!processedIndexes.includes(j) && numbers[j] < minNumber) {
+        minNumber = numbers[j]
+        minNumberIndex = j
+      }
+    }
+    
+    // if we already have numbers then add minimum one to previous sum
+    if (sums.length) {
+      sums.push(sums[sums.length - 1] + minNumber)
+    } else {
+      sums.push(minNumber)
+    }
+    
+    processedIndexes.push(minNumberIndex)
+  }
+  
+  return sums
+}
+
+// console.log(findSumOfNumbers())
+
+/*
  * 22.07.21
  * You open your own radio program and want to be
  * heard in all 50 states. You need to decide on
@@ -49,7 +174,7 @@ function findMinNumberStationsToShowMyProgram() {
   return finalStations;
 }
 
-findMinNumberStationsToShowMyProgram();
+// findMinNumberStationsToShowMyProgram();
 
 /*
  * 22.07.2021
@@ -105,7 +230,6 @@ function findBestOfferForBoss(radioStations, budget) {
     budget,
   };
   
-  //
   for (let radioStation in radioStations) {
     if (!radioStations.hasOwnProperty(radioStation)) {
       return;
---
+++ ds/linked-list.js
@@ -0,0 +1,153 @@
+// https://www.youtube.com/watch?v=ZBdE8DElQQU
+
+class Node {
+  constructor(val, next = null) {
+    this.val = val
+    this.next = next
+  }
+}
+
+
+// TODO:
+// 1) Remove/add last
+const n1 = new Node(100)
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.size = 0
+  }
+  
+  // Inset first node
+  insetFirst(val) {
+    this.head = new Node(val, this.head)
+    this.size++
+  }
+  
+  // Insert last node
+  insertLast(val) {
+    let node = new Node(val)
+    let current
+    
+    // if empty, make head
+    if (!this.head) {
+      this.head = node
+    } else {
+      current = this.head
+      
+      while (current.next) {
+        current = current.next
+      }
+      
+      current.next = node;
+    }
+    
+    this.size++;
+  }
+  
+  // Insert at index
+  insertAt(val, index) {
+    // if index out of range
+    if (index < 0 || index >= this.size) {
+      return null
+    }
+    
+    if(index === 0) {
+      this.head = new Node(val, this.head)
+    }
+    
+    let current, previous;
+    
+    // Set current to first
+    current = this.head
+    let count = 0
+    
+    while(count < index) {
+      previous = current
+      count++
+      current = current.next
+    }
+    
+    const node = new Node(val, current)
+    previous.next = node
+    this.size++
+  }
+  
+  // Get at index
+  getAt(index) {
+    let current = this.head
+    let count = 0
+    
+    if (index < 0 || index >= this.size) {
+      return null
+    }
+    
+    while (current) {
+      if (count === index) {
+        console.log(current.val)
+        return current.val
+      }
+      count++
+      current = current.next
+    }
+  }
+  
+  // Remove at index
+  removeAt(index) {
+    if (index < 0 || index >= this.size) {
+      return null
+    }
+    
+    let current = this.head
+    let previous
+    let count = 0
+    
+    if (index === 0) {
+      console.log(this.head.val)
+      this.head = this.head.next
+      return
+    }
+    
+    while (count < index) {
+      previous = current
+      current = current.next
+      count++
+    }
+    
+    console.log(current.val)
+    previous.next = current.next
+    this.size--
+  }
+  
+  // Clear the list
+  clearList() {
+    this.head = null
+    this.size = 0
+  }
+  
+  // Print the list data
+  printListData() {
+    let current = this.head
+    
+    if (!current) {
+      console.log('empty list')
+    }
+    
+    while (current) {
+      console.log(current.val)
+      current = current.next
+    }
+  }
+}
+
+const ll = new LinkedList()
+
+ll.insetFirst(100)
+ll.insetFirst(200)
+ll.insetFirst(300)
+// ll.insertAt(400, 2)
+ll.removeAt(1)
+ll.clearList()
+
+ll.printListData()
